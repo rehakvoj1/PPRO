b901f461120a60a63e144def77aeb4f42b5f4b75
Vojtěch Řehák <rehakvoj1@gmail.com>
2021-01-10 22:24:20 +0100

    basic image handling

--- PPRO/src/Widgets/HangmanGame.cpp
+++ PPRO/src/Widgets/HangmanGame.cpp
@@ -11,6 +11,16 @@ HangmanGame::HangmanGame() {
 	addWidget( std::move( title ) );
 	addWidget( std::make_unique<Wt::WAnchor>( Wt::WLink( Wt::LinkType::InternalPath, "/list" ), "list" ) );
 
+	m_hangmanImg = new Wt::WStackedWidget();
+	for ( int i = 0; i < 12; ++i ) {
+		std::string name = "resources/hangman" + std::to_string( i ) + ".png";
+		Wt::WImage* image = m_hangmanImg->addNew<Wt::WImage>( Wt::WLink( name ) );
+		image->setAlternateText( "HANGMAN" );
+		image->resize( 500, 500 );
+		m_images.emplace_back( image );
+	}
+	addWidget( std::unique_ptr<Wt::WStackedWidget>( m_hangmanImg ) );
+
 	std::istringstream ss( tr( "hangman.letters" ).toUTF8() );
 	std::string letter;
 
--- PPRO/src/Widgets/HangmanGame.h
+++ PPRO/src/Widgets/HangmanGame.h
@@ -1,11 +1,15 @@
 #pragma once
 #include <Wt/WContainerWidget.h>
+#include <Wt/WImage.h>
+#include <Wt/WStackedWidget.h>
+#include <vector>
 
 class HangmanGame : public Wt::WContainerWidget {
 public:
 	HangmanGame();
 
 private:
-
+	Wt::WStackedWidget* m_hangmanImg;
+	std::vector<Wt::WImage*> m_images;
 };
 
